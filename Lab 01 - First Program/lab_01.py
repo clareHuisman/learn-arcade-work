a62713f6c03737512bd2f809808f5141310e0cd4
Clarissa Huisman <clarissahuisman@clarissas-air.sc.loc>
2020-08-27 14:26:56 -0500

    lab_01

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,7 @@
+print("We don't need no education\n"
+      "We don't need no thought control\n"
+      "No dark sarcasm in the classroom\n"
+      "Teachers leave them kids alone\n"
+      "Hey, teachers, leave them kids alone\n"
+      "\tAll in all it's just another brick in the wall\n"
+      "\tAll in all you're just another brick in the wall\n")
